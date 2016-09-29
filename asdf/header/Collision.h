19ab4273f043304b0ac1f5570da88702d4dcdcd5
Alex Rehnby-Martin <alrm014@gmail.com>
2016-09-29 13:16:04 -0700

    commit for transfer

--- asdf/header/Collision.h
+++ asdf/header/Collision.h
@@ -1,11 +1,17 @@
 #include "Transform.h"
 #include <glm/vec3.hpp>
+#include <vector>
 
 class Collision
 {
 public:
   Collision(){};
   Collision(Transform* transformSystem){ _transformSystem = transformSystem; };
+
+  struct Data
+  {
+    std::vector<glm::vec3> sizes;
+  };
 
   void addBoxCollider(glm::vec3 size, glm::vec3 offset = glm::vec3(0,0,0));
   void updateCollisions(Uint32 deltaTime){};
